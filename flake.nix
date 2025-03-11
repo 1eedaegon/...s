68cb0f6dba1b3a7e511c2813a06000abfad80488
1eedaegon <d8726243@gmail.com>
2025-03-11 10:44:42 +0900

    feat: add common shell hook

--- flake.nix
+++ flake.nix
@@ -26,20 +26,11 @@
           curl
           asciinema
         ];
-        
-        # lib = import (self + "/lib") {inherit pkgs; };
-        
-        # Call generating
-        # commonShellHooks = pkgs.callPackage ./lib/common-shell-hook.nix { inherit pkgs; };
-        commonShellHooks = ''
-          eval "$(starship init bash)"
-          alias l="ls -lah"
-          alias cat="bat"
-        '';
-         
+        commonShellHooks = import ./lib/common-shell-hook.nix { inherit pkgs; };
+
+        # Not yet: https://github.com/NixOS/nix/pull/8901 
         # ++ (if stdenv.isWindows then [ chocolatey ] else []);
-        # commonHooks = import "./lib/common-shell-hook.nix";
-        # Not yet: https://github.com/NixOS/nix/pull/8901
+        
         # CLI 바이너리 처리
         # myCliBinary = import ./cli-derivation.nix { inherit pkgs system; };
         # hasBinary = myCliBinary ? package && myCliBinary.package != null;
@@ -61,7 +52,8 @@
             lldb
           ] ++ commonPkgs;
           
-          shellHook = commonShellHooks+''
+          shellHook = ''
+            ${commonShellHooks}
             # Log level
             declare -x name="rust"
             export RUST_BACKTRACE=1
--- lib/common-shell-hook.nix
+++ lib/common-shell-hook.nix
@@ -1,18 +1,5 @@
+# 기본 shll 설정을 위한 shellHook
 { pkgs ? import  {} }:
-
-{
-  # 기본 shll 설정을 위한 shellHook
-  starshipHook = ''
-    eval "$(starship init bash)"
-
-  '';
-
-  # nodeAndGitHook = ''
-  #   export EDITOR=vim
-  #   export PATH=$PATH:$PWD/node_modules/.bin
-
-  #   git config --local core.editor vim
-  #   echo "Current dir: $(pwd)"
-  # '';
-  
-}
+''
+  eval "$(starship init bash)"
+''
