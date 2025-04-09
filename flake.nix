0360f254d4f4459c3d0397f5ea3eab5a071da222
1eedaegon <d8726243@gmail.com>
2025-04-09 13:07:23 +0900

    feat: add ultimate python package manager

--- flake.nix
+++ flake.nix
@@ -5,11 +5,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   # TODO: Define packages and apps for default environment
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, home-manager, rust-overlay, ... }:
      flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -25,6 +31,9 @@
           git
           curl
           asciinema
+          fontconfig
+          direnv
+          uv
         ];
         commonShellHooks = import ./lib/common-shell-hook.nix { inherit pkgs; };
 
@@ -99,6 +108,9 @@
             alias cb='cargo build'
             alias ct='cargo test'
             alias cr='cargo run'
+            
+            # Version
+            rustc --version
           '';
         };
         
@@ -120,9 +132,24 @@
             export GOPATH="$HOME/go"
             export PATH="$GOPATH/bin:$PATH"
             mkdir -p $GOPATH
+
+            # Version
+            go version
           '';
         };
         
+        # Python Dev
+        pyLatest = pkgs.mkShell {
+	  name = "py";
+          buildInputs = with pkgs; [
+            uv
+          ] ++ commonPkgs;
+          
+          shellHook = commonShellHooks+ ''
+            # Version
+            uv version
+          '';
+        };
 
         # Default, 
         devShell = pkgs.mkShell {
@@ -138,13 +165,24 @@
           '';
         };
       
-      in {
-        devShells = {
+      in with pkgs; {
+        # homeConfigurations."1eedaegon-${system}" = home-manager.lib.homeManagerConfiguration {
+        #     modules = [
+        #       {
+        #         home.username = "1eedaegon";
+        #         home.homeDirectory = "/home/1eedaegon";
+        #         home.stateVersion = "22.11";
+        #         programs.home-manager.enable = true;
+        #       }
+        #     ];
+        #   };
+        packages = {
           default = devShell;
           dev = devShell;
           rust = rustLatest;
           rust170 = rust_1_70_0;
           go = goLatest;
+          py = pyLatest;
         };
       }
     );
