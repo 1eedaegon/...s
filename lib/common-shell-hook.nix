53757dc8b4f784161d4fb53c40ca3f0d7ed9c926
1eedaegon <d8726243@gmail.com>
2025-04-14 17:56:41 +0900

    feat: add initialize pastel theme

--- lib/common-shell-hook.nix
+++ lib/common-shell-hook.nix
@@ -2,7 +2,11 @@
 { pkgs }:
 ''
   eval "$(starship init bash)"
+  if [ ! -f ~/.config/starship.toml ]; then
+    starship preset pastel-powerline -o ~/.config/starship.toml
+    
+  fi
   alias l="ls -lah"
-  echo -e "font check(branch): \ue0a0 \uf126 \ue0a0 \uf121"
+  echo -e "font check(branch): \uf126 \ue0a0 \uf121"
 
 ''
