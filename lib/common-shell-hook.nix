2c60dae3765a8bdbefc2c5033953c2f97680350a
1eedaegon <d8726243@gmail.com>
2025-04-18 18:56:03 +0900

    fix: change alias nix command

--- lib/common-shell-hook.nix
+++ lib/common-shell-hook.nix
@@ -6,7 +6,13 @@
     starship preset nerd-font-symbols -o ~/.config/starship.toml
   fi
   alias l="ls -lah"
+  
+  # Nix alias
   alias nd="nix devleop"
+  alias np="nix profile"
+  alias ncg="nix-collect-garbage"
+
+  # Kube alias
   alias k="kubectl"
   echo -e "font check(branch): \uf126 \ue0a0 \uf121"
 
