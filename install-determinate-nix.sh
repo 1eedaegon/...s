c75b65ba520a1548a3f6ca12321aee999812e78e
1eedaegon <d8726243@gmail.com>
2025-05-13 12:06:13 +0900

    feat: add install/uninstall bash

---
+++ install-determinate-nix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate
---
+++ uninstall-nix.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate
+sudo /nix/nix-installer uninstall
