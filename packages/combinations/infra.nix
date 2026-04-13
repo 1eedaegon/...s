1808135340a81e92c46ed59f18495438d7c9d92d
Gon ⌬ <d8726243@gmail.com>
2026-04-13 11:38:36 +0900

    feat: add iac tools

--- packages/combinations/infra.nix
+++ packages/combinations/infra.nix
@@ -1,5 +1,5 @@
 # packages/combinations/infra.nix
-# Go + Node.js (infrastructure tooling)
+# Go + Node.js + IaC tooling (terraform/opentofu side-by-side, helm, nats cli, vault)
 { pkgs }:
 
 let
@@ -7,5 +7,28 @@ let
   node = import ../toolchains/node.nix { inherit pkgs; };
 in
 {
-  packages = go.packages ++ node.packages;
+  packages = go.packages ++ node.packages ++ (with pkgs; [
+    # IaC (terraform + opentofu side-by-side)
+    terraform
+    opentofu
+    terragrunt
+    vault
+    ansible
+    packer
+    pulumi
+
+    # Kubernetes
+    kubectl
+    kubernetes-helm
+    helmfile
+    k9s
+    kind
+    minikube
+    stern
+    kubectx
+    kustomize
+
+    # Messaging
+    natscli
+  ]);
 }
